1f801094202f4707fffea5e1a5db5cdd9a9d9767
Maksym32 <bednogo1@gmail.com>
2022-07-22 21:20:56 +0300

    react_hw1_profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types'
+import css from './Profile.module.css'
+
+export const Profile = ({ username, tag, location, avatar, stats: { followers, views, likes } }) => {
+  return <div className={css.profile}>
+  <div className={css.description}>
+    <img
+      src={avatar}
+      alt="User avatar"
+      className={css.avatar}
+    />
+    <p className={css.name}>{username}</p>
+      <p className={css.tag}>{tag}</p>
+    <p className={css.location}>{location}</p>
+  </div>
+
+  <ul className={css.stats}>
+    <li className={css.statsItem}>
+      <span className={css.label}>followers</span>
+      <span className={css.quantity}>{followers}</span>
+    </li>
+    <li className={css.statsItem}>
+        <span className={css.label}>views</span>
+      <span className={css.quantity}>{views}</span>
+    </li>
+    <li className={css.statsItem}>
+      <span className={css.label}>likes</span>
+      <span className={css.quantity}>{likes}</span>
+    </li>
+  </ul>
+</div>
+}
+
+Profile.prototypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number),
+}
